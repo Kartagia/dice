7df4ee133423981e7f35782eea86a968dfef3dfe
Antti Kautiainen <antti@kautiainen.com>
2024-12-01 09:17:00 +0200

    doc: class SimpleDieTest
    Added JDoc documentation of the test.

--- src/test/java/com/kautiainen/antti/rpgs/dice/model/SimpleDieTest.java
+++ src/test/java/com/kautiainen/antti/rpgs/dice/model/SimpleDieTest.java
@@ -10,15 +10,36 @@ import org.junit.jupiter.api.Test;
 
 public class SimpleDieTest {
 
+    /**
+     * The error message constant indicating the test failed due result value was not within the side container.
+     */
+    public static final String ROLL_RESULT_VALUE_WAS_NOT_WITHIN_THE_SIDE_CONTAINER = "Roll result value was not within the side container";
+
     @Test
     void construction() {
 
     }
 
+    /**
+     * Convert a list into a list of strings.
+     * 
+     * @param <E> The list item type.
+     * @param list The converted list.
+     * @param stringifier The function converting the list items.
+     * @return The list containing string representations of the list.
+     */
     public static<E> List<String> toStringList(List<? extends E> list, Function<? super E, ? extends CharSequence> stringifier) {
         return list.stream().map(item -> (item == null ? null : stringifier.apply(item).toString())).toList();
     }
 
+    /**
+     * Convert a list into a human readable string representation.
+     * 
+     * @param <E> The list item type.
+     * @param list The converted list.
+     * @param stringifier The function converting the list items.
+     * @return The string representation of the list.
+     */
     public static<E> String listToString(List<? extends E> list, Function<? super E, ? extends CharSequence> stringifier) {
         StringBuilder result = new StringBuilder();
         CharSequence[] strings = list.stream().map(stringifier).toArray( count -> (new String[count]));
@@ -65,7 +86,7 @@ public class SimpleDieTest {
         for (int i=0; i < dice.size(); i++) {
             for (int attempt=0; attempt < 1000; attempt++) {
                 DieResult<Integer> result = dice.get(i).getResult();
-                assertTrue(sides.get(i).contains(result.getValue()), "Roll result value was not within the side container");
+                assertTrue(sides.get(i).contains(result.getValue()), ROLL_RESULT_VALUE_WAS_NOT_WITHIN_THE_SIDE_CONTAINER);
             }
         }
 
