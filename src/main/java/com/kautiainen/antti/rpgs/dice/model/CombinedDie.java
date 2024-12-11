d85ac4c634630810f33e604955e7e3d05ba27f32
Antti Kautiainen <antti@kautiainen.com>
2024-12-11 23:13:07 +0200

    fix: implement Die change on ComplexDie
    Changes to be committed:
    	modified:   src/main/java/com/kautiainen/antti/rpgs/dice/model/CombinedDie.java
        - fix: class ComplexDie<T,V>
            - feat: method getSides() returning the sides of the complex die.
            Sides contains all combinations of results of the complex die.

--- src/main/java/com/kautiainen/antti/rpgs/dice/model/CombinedDie.java
+++ src/main/java/com/kautiainen/antti/rpgs/dice/model/CombinedDie.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collector;
 
-
 /**
  * A combined die is a set of dice acting as a die.
  */
@@ -17,7 +16,7 @@ public class CombinedDie<T, V> implements Dice<T>, Die<V> {
     /**
      * Create combined die from roll result.
      * 
-     * @param 
+     * @param
      */
     public CombinedDie(RollResult<T, V> result) {
         this.combiner = result.getCombiner();
@@ -27,29 +26,29 @@ public class CombinedDie<T, V> implements Dice<T>, Die<V> {
     /**
      * Create a new combined die.
      * 
-     * @param dice The members of the combined dice. 
+     * @param dice The members of the combined dice.
      */
     public CombinedDie(
-        Collection<Die<? extends T>> dice,
+            Collection<Die<? extends T>> dice,
             Collector<? super T, ?, ? extends V> combiner) {
 
         this.dice = new ArrayList<>(dice);
         this.combiner = combiner;
     }
 
-    public<A> CombinedDie(
-        List<Die<T>> dice, 
-        Collector<? super T, A, ? extends V> combiner) {
+    public <A> CombinedDie(
+            List<Die<T>> dice,
+            Collector<? super T, A, ? extends V> combiner) {
         this.dice = new ArrayList<>(dice.size());
-        for (Die<T> die: dice) {
+        for (Die<T> die : dice) {
             this.dice.add(die);
         }
         this.combiner = combiner;
     }
 
     public CombinedDie(
-        Dice<? extends T> dice,
-        Collector<? super T, ?, ? extends V> combiner) {
+            Dice<? extends T> dice,
+            Collector<? super T, ?, ? extends V> combiner) {
         this(new ArrayList<>(dice.getDice()), combiner);
     }
 
@@ -91,4 +90,37 @@ public class CombinedDie<T, V> implements Dice<T>, Die<V> {
             return result;
         }).toList();
     }
+
+    @Override
+    public List<? extends V> getSides() {
+        // Generating the combinations table for the dice.
+        Collector<List<? extends T>, ListOfList<T>, List<? extends V>> sideCombiner = Collector.of(
+                () -> (new ListOfList<T>()),
+                (ListOfList<T> head, List<? extends T> sides) -> {
+                    ListOfList<T> newSidesResult = new ListOfList<>(head.size() * sides.size());
+                    for (List<T> headSides : head) {
+                        for (T side : sides) {
+                            ArrayList<T> newSides = new ArrayList<>(headSides);
+                            newSides.add(side);
+                            newSidesResult.add(newSides);
+                        }
+                    }
+                    head.clear();
+                    head.addAll((Collection<List<T>>)newSidesResult);
+                },
+                (ListOfList<T> head, ListOfList<T> tail) -> {
+                    return ListOfList.combine(head, tail);
+                },
+                (ListOfList<T> head) -> {
+                    List<V> result = new ArrayList<>(
+                            head.stream().map((List<T> sides) -> ((V) sides.stream().collect(getCombiner()))).toList()
+                    );
+                    return result;
+                });
+        return getDice().stream().map(die -> {
+            List<? extends T> sides = die.getSides();
+            return sides;
+        }).collect(sideCombiner);
+    }
+
 }
